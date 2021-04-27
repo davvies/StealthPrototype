19fae9a83508ebaf2605ae75eba096909927dd66
davvies <davies1044@outlook.com>
2021-04-27 15:46:32 +0100

    Agent copyright removal

--- Source/Stealth/Agent.cpp
+++ Source/Stealth/Agent.cpp
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "Agent.h"
 #include "Runtime/Engine/Classes/Engine/World.h"
 #include "GameFramework/PlayerController.h"
